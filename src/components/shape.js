3da94b9c463ef82f0b58ad3d9ff2cbc4d99164fb
ANSHU-1010 <94850667+ANSHU-1010@users.noreply.github.com>
2021-11-26 20:31:37 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,21 @@
 // declare class
+class Shape {
+    constructor() {  
+        this.color = "blue";    
+      
+    }
+    
+    // Method
+    calculateArea () { 
 
+    }
+
+    drawShape () {
+
+    } 
+  
+  }
 // export class using module.exports
+
+module.exports = Shape;
+
